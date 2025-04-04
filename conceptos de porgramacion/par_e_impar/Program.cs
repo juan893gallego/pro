91d715de1cd55e1aababbd070191422f49e7b4ef
juan893gallego <gallegoj987@gmail.com>
2025-04-04 11:30:39 -0500

    par e impar commit -m

---
+++ conceptos de porgramacion/par_e_impar/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("ingrese un numero,por favor");
+
+int numero;
+if (int.TryParse(Console.ReadLine(), out numero))
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($"El número es par ");
+
+    }
+    else
+    {
+        Console.WriteLine($"El número es impar");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada no valida");
+}
+
